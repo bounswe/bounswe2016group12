fce7e2bf6b646c38bea930ecfe5bba2e6c22459f
utkan <utkangezer@gmail.com>
2016-04-23 15:19:02 +0300

    Changes on maven module.

---
+++ maven_mod/src/main/java/edu/boun/cmpe451/App.java
@@ -0,0 +1,13 @@
+package edu.boun.cmpe451;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
