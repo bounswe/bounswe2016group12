35c678c02826454c343e3af6d6ce07b39666e983
fepersembe-VAIO <invader7474@gmail.com>
2016-11-09 15:02:49 +0300

    Android - Classes, Activities and custom  layouts are generated as empty

---
+++ ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/SearchActivity.java
@@ -0,0 +1,13 @@
+package bounswe16group12.com.meanco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
