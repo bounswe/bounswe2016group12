38631bdc6565537d756d44e9f487f5484c186692
ezgitek <tekdemirezg@gmail.com>
2016-11-15 00:44:57 +0300

    ANDROID - front end add topic page

--- ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/HomeActivity.java
+++ ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/HomeActivity.java
@@ -1,29 +1,123 @@
 package bounswe16group12.com.meanco;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import me.originqiu.library.EditTag;
+import me.originqiu.library.MEditText;
+
 
 public class HomeActivity extends AppCompatActivity {
 
+static String text = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        final FloatingActionButton topic_fab = (FloatingActionButton) findViewById(R.id.add_topic) ;
+        topic_fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                View input = getLayoutInflater().inflate(R.layout.customview_alerttopic, null, false);
+                final EditTag editTagView = (EditTag) findViewById(R.id.edit_tag_view);
+                final MEditText mEditText = (MEditText) editTagView.findViewById(R.id.meditText);
+                mEditText.addTextChangedListener(new TextWatcher() {
+
+                    @Override
+                    public void onTextChanged(CharSequence returnedResult, int start,
+                                              int before, int count) {
+
+                        String result = returnedResult.toString();
+                        if(result.length() == 0) return;
+                        if(result.charAt(result.length() - 1)==(',')) // last character is ','
+                        {
+                            // comma is entered
+                            editTagView.addTag(result.substring(0, result.length() - 1));
+                            mEditText.setText("");
+                        }
+                    }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count,
+                                                  int after) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+
+                new AlertDialog.Builder( HomeActivity.this )
+                        .setTitle( "Add topic" )
+                        .setView(input)
+                        .setPositiveButton( "Add", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //TODO: add to database
+                                //input.getText();
+                            }
+                        })
+                        .setNegativeButton( "Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        } )
+                        .show();
+
+
             }
+
         });
+
+
+
+
+        final FloatingActionButton tag_fab = (FloatingActionButton) findViewById(R.id.add_tag) ;
+        tag_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View input = getLayoutInflater().inflate(R.layout.customview_alerttopic, null, false);
+
+                new AlertDialog.Builder( HomeActivity.this )
+                        .setTitle( "Add" )
+                        .setView(input)
+                        .setPositiveButton( "Save", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.d( "AlertDialog", "Positive" );
+                            }
+                        })
+                        .setNegativeButton( "Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.d( "AlertDialog", "Negative" );
+                            }
+                        } )
+                        .show();
+            }
+
+        });
+
     }
 
 }
--- ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/fragments/home/HomeActivityFragment.java
+++ ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/fragments/home/HomeActivityFragment.java
@@ -1,17 +1,37 @@
 package bounswe16group12.com.meanco.fragments.home;
 
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.github.clans.fab.FloatingActionMenu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import bounswe16group12.com.meanco.R;
+import bounswe16group12.com.meanco.fragments.profile.BioFragment;
+import bounswe16group12.com.meanco.fragments.profile.NotificationsFragment;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class HomeActivityFragment extends Fragment {
+public class HomeActivityFragment extends Fragment{
 
     public HomeActivityFragment() {
     }
@@ -19,6 +39,22 @@ public class HomeActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        String[] arr = {"ezgi", "furkan", "caner erkin"};
+        List<String> topiclist = new ArrayList<String>(Arrays.asList(arr));
+
+        ArrayAdapter<String> mTopicAdapter = new ArrayAdapter<String>(
+                getActivity(), R.layout.fragment_listitem, R.id.topicitem, topiclist
+        );
+
+        ListView listView = (ListView) rootView.findViewById(R.id.content_home);
+        listView.setAdapter(mTopicAdapter);
+        return rootView;
     }
+
+
+
+
 }
