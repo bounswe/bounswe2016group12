d0ab7c4440c26e347f5b0249dcb1f084668ec7e4
fepersembe <invader7474@gmail.com>
2016-12-09 23:59:16 +0300

    Android - unneccesary things removed

--- ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/adapters/TagSearchAdapter.java
+++ ANDROID/Meanco/app/src/main/java/bounswe16group12/com/meanco/adapters/TagSearchAdapter.java
@@ -67,7 +67,7 @@ public class TagSearchAdapter extends ArrayAdapter<Tag>{
 
 
             checkedTextView  = (CheckedTextView) v.findViewById(R.id.checkedTextView);
-          
+
             if(TagSearchActivity.checkedTags.indexOf(t) != -1)
                 checkedTextView.setChecked(true);
 
@@ -80,17 +80,6 @@ public class TagSearchAdapter extends ArrayAdapter<Tag>{
 
 
             checkedTextView.setText(str);
-           /* checkedTextView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    checkedTextView.toggle();
-                    if(TagSearchActivity.checkedTags.indexOf(t) == -1)
-                        TagSearchActivity.checkedTags.add(t);
-                    else
-                        TagSearchActivity.checkedTags.remove(t);
-
-                }
-            });*/
 
         return v;
     }
