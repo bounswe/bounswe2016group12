724fc0b3011f0187e1e7bea725419e845cb93041
heobu <esma.ozelbas@boun.edu.tr>
2016-12-15 03:39:35 +0200

    I - 221 - Web - Add Tag inside Topic Page

--- WEB-BACKEND/Meanco/MeancoApp/views/Comment.py
+++ WEB-BACKEND/Meanco/MeancoApp/views/Comment.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from sys import platform as _platform
-from MeancoApp.models import Topic, Comment, OfTopic, Relation, ViewedTopic,Profile
+from MeancoApp.models import Topic, Comment, OfTopic, Relation, ViewedTopic,Profile, Tag
 
 def get_page(request, id):
 
@@ -20,10 +20,8 @@ def get_page(request, id):
     topic.save()
     comments = Comment.objects.filter(topic=topicId)
     oftopic = OfTopic.objects.filter(topic=topicId)
-    relationsFrom = Relation.objects.filter(topic_a=topicId)
-    relationsTo = Relation.objects.filter(topic_b=topicId)
 
     if _platform=="win32":
-        return render(request, 'MeancoApp\CommentList.html', {'topic':topic, 'relationsFrom':relationsFrom, 'relationsTo':relationsTo, 'comments':comments, 'oftopic':oftopic})
+        return render(request, 'MeancoApp\CommentList.html', {'topic':topic, 'comments':comments, 'oftopic':oftopic})
     else:
-        return render(request, 'MeancoApp/CommentList.html', {'topic':topic, 'relationsFrom':relationsFrom, 'relationsTo':relationsTo, 'comments':comments, 'oftopic':oftopic})
+        return render(request, 'MeancoApp/CommentList.html', {'topic':topic, 'comments':comments, 'oftopic':oftopic})
